9a9e0d99d9356418c46ae5a16ad5956ae06721d4
Levi Appenfelder <levi.appenfelder@gmail.com>
2019-12-03 16:00:57 -0700

    Start of About Page
    Redirects to /about as default route
    Photo added, resized, and positioned

--- leviapp/src/App.js
+++ leviapp/src/App.js
@@ -3,13 +3,21 @@ import { Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './SASS/App.sass';
 import Navigation from './Navigation';
+import About from './About'
 import Dev from './Dev'
 import Art from './Art'
 import Contact from './Contact'
+
+import { Redirect } from 'react-router'
+
 function App() {
   return (
     <div className="App">
      <Navigation />
+      <Route exact path="/">
+      {<Redirect to="/about" />}
+      </Route>     
+    <Route path="/about" component={About} />
      <Route path="/dev" component={Dev} />
      <Route path="/art" component={Art} />
      <Route path="/contact" component={Contact} />
