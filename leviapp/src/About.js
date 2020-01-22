bdacd071893ab9315616a29edab895b07ca54a51
Levi Appenfelder <levi.appenfelder@gmail.com>
2020-01-22 16:40:22 -0700

    Achievements and Unique section added

--- leviapp/src/About.js
+++ leviapp/src/About.js
@@ -6,10 +6,17 @@ import Photo from './img/Levi.png'
 import DPSLogo from './img/DPSLogo.png'
 import NCTILogo from './img/NCTILogo.png'
 import AdaptecLogo from './img/AdaptecLogo.png'
+
+import CAALogo from './img/CAALogo.png'
 import RRCCLogo from './img/RRCCLogo.png'
 import MSULogo from './img/MSULogo.png'
 import UCDLogo from './img/UCDLogo.png'
 import LambdaLogo from './img/LambdaLogo.png'
+
+import UniqueBrain from './img/UniqueBrain.png'
+import NumberSystem from './img/NumberSystem.png'
+import German from './img/German.png'
+import Chess from './img/Chess.png'
 
 
 import { Link, DirectLink, Element , Events, animateScroll, scrollSpy, scroller } from 'react-scroll'
@@ -130,22 +137,22 @@ class About extends Component {
                     </h2>
                     <h3 className='arrows arrows_1'>❯❯❯❯❯</h3>
 
-                    <h2 onClick={() => {this.scrollTo('quotes', -225)}} className='all_sections second'>
+                    <h2 onClick={() => {this.scrollTo('work', -200)}} className='all_sections second'>
                     Work Experience
                     </h2>
                     <h3 className='arrows arrows_2'>❯❯❯❯❯</h3>
-                    <h2 onClick={() => {this.scrollTo('reform', -225)}} className='all_sections third'>
+                    <h2 onClick={() => {this.scrollTo('education', -200)}} className='all_sections third'>
                     Education
                     </h2>
                     <h3 className='arrows arrows_3'>❯❯❯❯❯</h3>
 
-                    <h2 onClick={() => {this.scrollTo('automaton', -200)}} className='all_sections fourth'>
-                    Automaton
+                    <h2 onClick={() => {this.scrollTo('achieve', -200)}} className='all_sections fourth'>
+                    Achievements
                     </h2>
                     <h3 className='arrows arrows_4'>❯❯❯❯❯</h3>
 
-                    <h2 onClick={() => {this.scrollTo('schatz', -200)}} className='all_sections fifth'>
-                    Schatzinsel
+                    <h2 onClick={() => {this.scrollTo('unique', -200)}} className='all_sections fifth'>
+                    Unique
                     </h2>
                 </div>
                 <h1 className="QUOTES"> Levi Appenfelder </h1>
@@ -174,7 +181,7 @@ class About extends Component {
             <section>
             <div className="about-sect">
             <div className='left'>
-            <Element className='start-section' name="quotes" >
+            <Element className='start-section' name="work" >
             <h2>❮❯ Work Experience ❮❯</h2>
             </Element>
 
@@ -260,12 +267,12 @@ class About extends Component {
             <section>
             <div className="about-sect">
             <div className='left'>
-            <Element className='start-section' name="quotes" >
+            <Element className='start-section' name="education" >
             <h2>❮❯ Education ❮❯</h2>
             </Element>
 
               <p>
-              My education has focused on three areas of study: Graphic Design, Science, Web Development.
+              My education has focused on Graphic Design, Science, and Web Development.
               <br/>
               <br/>
               Lambda School was where I studied Web Development. I focused on three main categories: Frontend, Backend, Computer Science.
@@ -325,6 +332,137 @@ class About extends Component {
               <a className='web-link' href='https://levijonas.app/art'>To Art Portfolio</a>
               <a className='web-arrow' href='https://levijonas.app/art'>⟶</a>
             </div>
+            </div>
+            </div>
+            </div>
+            </section>
+
+            <section>
+            <div className="about-sect">
+            <div className='left'>
+            <Element className='start-section' name="achieve" >
+            <h2>❮❯ Achievements ❮❯</h2>
+            </Element>
+
+              <p>
+              I was honored to receive several awards for my efforts.
+              <br />
+              <br />
+              My latest achievement was from MSU Denver where I won the Provost's Award,
+              an Outstanding Graduate Award at my graduation. I was honored to give the student
+              address in December, 2016.
+              <br />
+              <br />
+              At RRCC, I was given the Outstanding Graduate of the Year Award. I spoke the student
+              address at my graduation in May, 2011. 
+              <br />
+              <br />
+              At CAA, I was the High School Valedictorian. My college advisor nominated me for the
+              National Scholar Award, and I was honored to be rewarded with it.                          
+              </p>
+
+              <div className='automaton-focus'>
+              <h4>❯❯ MSU Denver 2016 </h4>  
+              <p>
+              ❯ Provost's Award / Outstanding Graduate Award
+              </p>
+              
+              </div>
+
+            <div className='automaton-focus'>
+              <h4>❯❯ RRCC 2011 </h4>  
+              <p>
+              ❯ Outstanding Graduate of the Year Award
+              </p>
+            </div>
+
+            <div className='automaton-focus'>
+              <h4>❯❯ CAA 2008 </h4>  
+              <p>
+              ❯ High School Valedictorian
+              <br />
+              ❯ National Scholar Award
+              </p>
+            </div>
+            </div>
+
+            <div className="right">
+            <div className='tech-used'>
+                <div className='single-tech'>
+                <img src={MSULogo} />
+                </div>
+
+                <div className='single-tech'>
+                <img src={RRCCLogo} />
+                </div>
+
+                <div className='single-tech'>
+                <img src={CAALogo} />
+                </div>
+            </div>
+            </div>
+            </div>
+            </section>
+
+            <section>
+            <div className="about-sect">
+            <div className='left'>
+            <Element className='start-section' name="unique" >
+            <h2>❮❯ Unique ❮❯</h2>
+            </Element>
+
+              <p>
+              Here are a few unique facts about me.
+              </p>
+
+              <div className='automaton-focus'>
+              <h4>❯❯ Brain </h4>  
+              <p>
+              ❯ I am missing a third of my brain.
+              <br/>
+              ❯ Doctors said I would never walk or talk.
+              </p>
+              </div>
+
+            <div className='automaton-focus'>
+              <h4>❯❯ Number System </h4>  
+              <p>
+              ❯ Each number 0-99 has an image associated with it.
+              </p>
+            </div>
+
+            <div className='automaton-focus'>
+              <h4>❯❯ German </h4>  
+              <p>
+              ❯ I am faithful investing time in learning German.
+              </p>
+            </div>
+
+            <div className='automaton-focus'>
+              <h4>❯❯ Chess </h4>  
+              <p>
+              ❯ Chess is a hobby of mine.
+              </p>
+            </div>
+            </div>
+
+            <div className="right">
+            <div className='tech-used'>
+                <div className='single-tech'>
+                <img src={UniqueBrain} />
+                </div>
+
+                <div className='single-tech'>
+                <img src={NumberSystem} />
+                </div>
+
+                <div className='single-tech'>
+                <img src={German} />
+                </div>
+
+                <div className='single-tech'>
+                <img src={Chess} />
+                </div>
             </div>
             </div>
             </div>
