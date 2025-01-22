cad705caa241ccbb5863f3be295f0a26af78c68d
Levi Appenfelder <levij.app@gmail.com>
2025-01-22 01:08:47 -0700

    updated React and other fixes

--- leviapp/src/index.js
+++ leviapp/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Updated import for React 18
 import './SASS/index.sass';
-import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+// Use ReactDOM.createRoot() for React 18
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
